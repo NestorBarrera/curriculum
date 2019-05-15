b426239e02a5080fa7b5fd5cc63babc919448cb6
NestorBarrera <nestorcaton04@gmail.com>
2019-05-15 10:32:43 -0500

    first commit

---
+++ src/views/Home.js
@@ -0,0 +1,17 @@
+import React,{Component}from'react';
+class Home extends Component{
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                    
+                        <h1>Hola</h1>
+                    </div>
+                </div>
+            </div>
+            
+        );
+    }
+}
+export default Home;
